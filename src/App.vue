68a8cf5a0ac1e4dee0f8549e66742f2b46d33735
yans67 <1018743361@qq.com>
2018-02-02 00:21:30 +0800

    暂定：编写逻辑

--- src/App.vue
+++ src/App.vue
@@ -15,33 +15,103 @@
             <!-- Material Theme Navbar  暂时不知道有什么作用-->
             <!-- <f7-navbar v-if="$theme.material" title="test"></f7-navbar> -->
             <f7-list v-for="(item,index) in list" :key="item.name">
-              <!-- <f7-list-item :title=item.name @click="songClick(index)"></f7-list-item> -->
-              <f7-list-button @click="songClick(index)">{{item.name}}
+              <!-- <f7-list-item :title=item.name @click= "songClick(index)"></f7-list-item> -->
+              <f7-list-button @click="listPlay(index)">{{item.name}}
 
               </f7-list-button>
             </f7-list>
+
             <div class="playBtns">
-              <f7-button class="iconfont normal click icon-shangyishoushangyige" @click="preClick"></f7-button>
-              <f7-button :class="'iconfont large click ' + playPause" @click="playPauseClick"></f7-button>
-              <f7-button class="iconfont normal click icon-xiayigexiayishou" @click="nextClick"></f7-button>
+              <f7-button class="iconfont normal click icon-shangyishoushangyige" @click="prePlay()"></f7-button>
+              <f7-button :class="'iconfont large click ' + playPause" @click="pausePlay"></f7-button>
+              <f7-button class="iconfont normal click icon-xiayigexiayishou" @click="nextPlay()"></f7-button>
             </div>
 
-            <audio id="audio">
-              <source title="你在终点等我" data-img="http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg" url="http://link.hhtjim.com/163/431357712.mp3">
-              <source title="白昼之夜（纯音乐）" data-img="http://p1.music.126.net/he7P6CYpQmz8KqYSdULuOQ==/94557999994394.jpg" url="http://oc1475jft.bkt.clouddn.com/baizhouzhiye.mp3">
-              <source title="告白气球" data-img="http://p1.music.126.net/tlp3VWVQVe0Je1r-oHn91g==/17666952835430891.jpg" url="http://oc1475jft.bkt.clouddn.com/gaobaiqiqiu.mp3">
-              <source title="那就这样吧" data-img="http://p1.music.126.net/fYNa5MMN0KTIYP2KMpYObQ==/17923139044585525.jpg" url="http://link.hhtjim.com/163/28949843.mp3">
-              <source title="骄傲的少年" data-img="http://p1.music.126.net/Brn39jwEDNPVV6pNWcv_rA==/1391981724588577.jpg" url="http://link.hhtjim.com/163/408332757.mp3">
-              <source title="岁月神偷" data-img="http://p1.music.126.net/54wdQi_3rpjreY2oo2jb7w==/5998935441219557.jpg" url="http://link.hhtjim.com/163/28285910.mp3">
-            </audio>
+            <audio :src="currentPlay" controls="" autoplay="" :ref="player" preload="true"></audio>
 
-            
           </f7-page>
         </f7-pages>
       </f7-view>
     </f7-views>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPlay: '', // 当前正在播放
+      player: '', // audio dom
+      playIndex:0,
+      list: [
+        {
+          name: '你在终点等我',
+          url: 'http://oc1475jft.bkt.clouddn.com/baizhouzhiye.mp3',
+          image:
+            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
+        },
+        {
+          name: '白昼之夜',
+          url: 'http://oc1475jft.bkt.clouddn.com/baizhouzhiye.mp3',
+          image:
+            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
+        },
+        {
+          name: '告白气球',
+          url: 'http://oc1475jft.bkt.clouddn.com/gaobaiqiqiu.mp3',
+          image:
+            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
+        },
+        {
+          name: '那就这样吧',
+          url: 'http://link.hhtjim.com/163/28949843.mp3',
+          image:
+            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
+        },
+         {
+          name: '骄傲的少年',
+          url: 'http://link.hhtjim.com/163/408332757.mp3',
+          image:
+            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
+        }
+      ],
+      playPause: 'icon-bofang'
+    }
+  },
+  methods: {
+    listPlay(index) {
+      this.currentPlay = this.list[index].url
+      console.log(this.currentPlay)
+      // debugger
+      // this.player.play()
+    },
+
+    // note:播放，上一首，下一首的方法
+    // 播放暂停按钮
+    pausePlay() {
+      this.playPause =
+        this.playPause === 'icon-weibiaoti519'
+          ? 'icon-bofang'
+          : 'icon-weibiaoti519'
+      console.log(this.player.playing)
+    },
+    prePlay(index) {
+      console.log('pre')
+      this.currentPlay = this.list[--playIndex].url
+      
+    },
+    nextPlay(index) {
+      console.log('next')
+      this.currentPlay = this.list[++playIndex].url
+      
+    }
+  },
+  mounted(){
+    this.currentPlay = this.list[playIndex].url;
+  }
+}
+</script>
+
 
 <style>
 .normal {
@@ -54,9 +124,9 @@
   margin-right: 55px;
   min-width: 44px;
   display: inline-block;
-   /* background-color: rgba(101,0,0,1); */
+  /* background-color: rgba(101,0,0,1); */
   /* box-shadow: 1px 1px 2px #888888; */
-  background-color:  rgba(0, 0, 0, );
+  background-color: rgba(0, 0, 0, 0 0);
   /* background: #203;  */
   /* color: blue; */
 }
@@ -76,71 +146,13 @@
   display: inline-block;
 }
 .large:active {
-  background-color:  rgba(0, 0, 0, 0);
+  background-color: rgba(0, 0, 0, 0);
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  text-shadow: 0 0 .1em, 0 0 .3em;
-  
+  text-shadow: 0 0 0.1em, 0 0 0.3em;
 }
 .normal:active {
-   background-color:  rgba(0, 0, 0, 0);
+  background-color: rgba(0, 0, 0, 0);
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  text-shadow: 0 0 .1em, 0 0 .3em;
+  text-shadow: 0 0 0.1em, 0 0 0.3em;
 }
 </style>
-
-<script>
-export default {
-  data() {
-    return {
-      list: [
-        {
-          name: '1',
-          url: 'http://link.hhtjim.com/163/431357712.mp3',
-          image:
-            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
-        },
-        {
-          name: '2',
-          url: 'http://link.hhtjim.com/163/431357712.mp3',
-          image:
-            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
-        },
-        {
-          name: '3',
-          url: 'http://link.hhtjim.com/163/431357712.mp3',
-          image:
-            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
-        },
-        {
-          name: '4',
-          url: 'http://link.hhtjim.com/163/431357712.mp3',
-          image:
-            'http://p1.music.126.net/ddhcDeGSl9VhXJLfOsNDEA==/3433774824740403.jpg'
-        }
-      ],
-      playPause: 'icon-bofang'
-    }
-  },
-  methods: {
-    songClick(index) {
-      console.log(index)
-      let audio = document.getElementById('audio')
-      audio.play()
-    },
-
-    // note:播放，上一首，下一首的方法
-    playPauseClick() {
-      this.playPause =
-        this.playPause === 'icon-weibiaoti519'
-          ? 'icon-bofang'
-          : 'icon-weibiaoti519'
-    },
-    preClick() {
-      console.log('pre')
-    },
-    nextClick() {
-      console.log('next')
-    }
-  }
-}
-</script>
