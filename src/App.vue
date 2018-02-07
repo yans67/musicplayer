84f42bf2fbd0f620883c32c6f065350fbd849324
yans67 <1018743361@qq.com>
2018-02-07 18:24:31 +0800

    完成大致样式，编写歌曲列表

--- src/App.vue
+++ src/App.vue
@@ -8,54 +8,97 @@
         <div class="navbar">
           <div style="height:0px" class="navbar-inner">
             <!-- Sub navbar -->
-            <div style="height:64px" class="subnavbar">
+            <div class="subnavbar music-nav">
               <div class="buttons-row">
-                <a href="#tab1" class="button tab-link active">歌手</a>
-                <a href="#tab2" class="button tab-link">歌曲</a>
-                <a href="#tab3" class="button tab-link">收藏</a>
+                <a href="#tab1" class="button tab-link active">1</a>
+                <a href="#tab2" class="button tab-link">2</a>
+                <a href="#tab3" class="button tab-link">3</a>
               </div>
             </div>
           </div>
         </div>
 
-    
+        <div class="pages">
+          <div data-page="home" class="page pad-top-nav" :style="playPanelHeight">
 
-        <div class="pages navbar-through">
-           
-          <div data-page="home" class="page with-subnavbar" style="height:500px">
-          
-            <div class="page-content hide-bars-on-scroll">
+            <div class="page-content">
               <div class="tabs">
+
                 <div id="tab1" class="tab active">
-                  <div class="content-block">
-                    <p>Lorem ipsum dolor ...</p>
-                    <p>In sed augue non ...</p>
+
+                  <div class="list-block contacts-block">
+                     <ul>
+                    <li>
+                      <div class="item-content">
+                        <div class="item-inner">
+                          <div class="item-title">Aaron </div>
+                        </div>
+                      </div>
+                    </li>
+                     </ul>
                   </div>
+
                 </div>
+
                 <div id="tab2" class="tab">
-                  <div class="content-block">
                     <p>Donec iaculis ...</p>
                     <p>Curabitur egestas, mi ...</p>
                     <p>Donec iaculis posuere ...</p>
-                  </div>
                 </div>
+
                 <div id="tab3" class="tab">
-                  <div class="content-block">
                     <p>Etiam non interdum erat...</p>
                     <p>Duis ac semper risus. Suspendisse...</p>
                     <p>Etiam non interdum erat...</p>
                     <p>Duis ac semper risus. Suspendisse...</p>
-                  </div>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                     <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
+                    <p>Etiam non interdum erat...</p>
+                    <p>Duis ac semper risus. Suspendisse...</p>
                 </div>
+
               </div>
             </div>
 
             <div class="playPanel">
-          </div>
+              <a href="#" class="iconfont icon-loop-r small"></a>
+              <a href="#" class="iconfont icon-pre normal"></a>
+              <a href="#" class="iconfont icon-play large"></a>
+              <a href="#" class="iconfont icon-next normal"></a>
+              <a href="#" class="iconfont icon-marked small"></a>
+            </div>
+
           </div>
         </div>
 
-       
       </div>
     </div>
   </div>
@@ -70,6 +113,10 @@ export default {
       playIndex: 0, // 播放歌曲下标
       loop: false, // 默认不循环
       playStyle: 'icon-bofang', // 播放暂停的样式
+      playPanelHeight: {
+        // 音乐播放面板的高度
+        height: window.innerHeight - 119 + 'px'
+      },
       list: [
         {
           name: '白昼之夜',
@@ -153,31 +200,34 @@ export default {
   font: 1.2em sans-serif;
 } */
 
-.normal {
-  font-size: 44px;
-  width: 44px;
-  height: 44px;
-  line-height: 44px;
+.small {
+  font-size: 24px;
+  width: 24px;
+  height: 24px;
+  line-height: 24px;
   padding: 0px;
-  margin-left: 55px;
-  margin-right: 55px;
-  min-width: 44px;
+  min-width: 24px;
+  display: inline-block;
+  margin-left: 25px;
+  margin-right: 25px;
+}
+.normal {
+  font-size: 38px;
+  width: 38px;
+  height: 38px;
+  line-height: 38px;
+  padding: 0px;
+  margin-left: 28px;
+  margin-right: 28px;
+  min-width: 38px;
   display: inline-block;
   /* background-color: rgba(101,0,0,1); */
   /* box-shadow: 1px 1px 2px #888888; */
-  background-color: rgba(0, 0, 0, 0 0);
+  /* background-color: rgba(0, 0, 0, 0 0); */
   /* background: #203;  */
   /* color: blue; */
 }
-.playPanel {
-  height: 119px;
-  width: 100%;
-  background: blue;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 20px;
-}
+
 .large {
   font-size: 64px;
   width: 64px;
@@ -196,5 +246,28 @@ export default {
   background-color: rgba(0, 0, 0, 0);
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   text-shadow: 0 0 0.1em, 0 0 0.3em;
+}
+
+.pad-top-zero {
+  padding-top: 0px;
+}
+
+.pad-top-nav {
+  padding-top: 62px;
+}
+
+.playPanel {
+  height: 119px;
+  width: 100%;
+  background: #f0f4f9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.music-nav {
+  height: 64px;
+  background: #f0f4f9;
 }
 </style>
